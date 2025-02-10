9dcc0bbb66bd15ebb8e7dde2d30c1992de977cd3
h-nakashima <h-nakashima@sofnet-pro.co.jp>
2025-02-10 16:53:53 +0900

    editBoard

--- frontend/features/editBoard/editBoardClient.tsx
+++ frontend/features/editBoard/editBoardClient.tsx
@@ -11,11 +11,14 @@ import { useMenu } from "@/app/contexts/menuContext";
 import Input from "@/app/components/input";
 import { useState } from "react";
 import ConfirmModal from "@/app/components/confirmModal";
+// import { Select } from "@/app/components/select";
 
 const EditBoardClient = () => {
   const router = useRouter();
   const { setMenuId } = useMenu();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [editModalOpen, setEditModalOpen] = useState<boolean>(false);
+  // const [selectedItem, setSelectedItem] = useState<{ name: string; product: string }>({ name: "", product: "" });
 
   const Redirect = (route: string) => {
     router.push(route);
@@ -78,13 +81,17 @@ const EditBoardClient = () => {
               <Button
                 label="編集"
                 className="btn btn-xs btn-accent w-16 mr-3"
+                onClick={() => {
+                  // setSelectedItem({ name: `R${index + 1}`, product: "Product 1" });
+                  setEditModalOpen(true);
+                }}
               />
               <Button
                 label="削除"
                 className="btn btn-xs btn-secondary w-12 mr-5"
                 onClick={() => setModalOpen(true)}
               />
-              <h1 className="font-bold">R1</h1>
+              <h1 className="font-bold">R{index + 1}</h1>
               <h1 className="font-bold ml-5">20Ω</h1>
               <h1 className="font-bold ml-5">
                 Resistor_SMD:R_0603_1608Metric_Pad1.05x0.95mm_HandSolder
@@ -117,11 +124,51 @@ const EditBoardClient = () => {
         isOpen={modalOpen}
         title="素子削除"
         body="本当に削除しますか？"
-        onConfirm={() => {
-          setModalOpen(false);
-        }}
+        onConfirm={() => setModalOpen(false)}
         onCancel={() => setModalOpen(false)}
       />
+
+      {editModalOpen && (
+        <div className="modal modal-open">
+          <div className="modal-box max-w-5xl">
+            <div className="flex flex-col bg-base-300 rounded-box p-3">
+              <h2 className="font-bold text-lg">素子編集</h2>
+              <h2 className="font-bold text-lg">素子編集</h2>
+              <h2 className="font-bold text-lg">素子編集</h2>
+              <h2 className="font-bold text-lg">素子編集</h2>
+              <h2 className="font-bold text-lg">素子編集</h2>
+              <h2 className="font-bold text-lg">素子編集</h2>
+              <h2 className="font-bold text-lg">素子編集</h2>
+              <label className="block mt-3">名前</label>
+              {/* <Input
+              type="text"
+              value={selectedItem.name}
+              onChange={(e) => setSelectedItem({ ...selectedItem, name: e.target.value })}
+              className="input input-bordered w-full"
+            /> */}
+              <label className="block mt-3">製品</label>
+              {/* <Select
+              options={["Product 1", "Product 2", "Product 3"]}
+              value={selectedItem.product}
+              onChange={(value) => setSelectedItem({ ...selectedItem, product: value })}
+              className="select select-bordered w-full"
+            /> */}
+            </div>
+            <div className="flex justify-center mt-3 modal-action">
+              <Button
+                label="戻る"
+                className="btn btn-outline btn-secondary"
+                onClick={() => setEditModalOpen(false)}
+              />
+              <Button
+                label="変更"
+                className="btn btn-primary ml-10 w-32"
+                onClick={() => setEditModalOpen(false)}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
