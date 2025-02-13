c452f40df7d5ae3cac5bae38bb7600b697315f2a
h-nakashima <h-nakashima@sofnet-pro.co.jp>
2025-02-13 23:16:34 +0900

    update frontend layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,7 @@ const RootLayout = ({
       <body>
         <MenuProvider>
           <NavigationArea />
-          <main className=" z-0 mt-[60px] md:mt-[110px] mb-[80px] pr-5 pl-5">
+          <main className="z-0 mt-[60px] md:mt-[110px] mb-[80px] pr-5 pl-5">
             {children}
           </main>
         </MenuProvider>
